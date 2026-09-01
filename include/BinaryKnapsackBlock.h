5b7e778b1670626738512033bb162d6ab4ec199e
Donato Meoli <donato.meoli.95@gmail.com>
2026-09-01 21:17:14 +0200

    the Solution version of is_feasible() and is_optimal() has a name of its own, so overriding one does not hide the other

--- include/BinaryKnapsackBlock.h
+++ include/BinaryKnapsackBlock.h
@@ -520,8 +520,6 @@ class BinaryKnapsackBlock : public Block {
  bool is_feasible( bool useabstract = false , 
                    Configuration * fsbc = nullptr ) override;
 
- /// the Solution version is not hidden by the one above [see Block]
- using Block::is_feasible;
 
 /*--------------------------------------------------------------------------*/
  /// returns true if the Binary Knapsack problem is empty.
